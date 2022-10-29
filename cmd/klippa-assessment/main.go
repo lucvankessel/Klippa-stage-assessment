f0ceab2cad14418694cdefc3ca102cdd394b3e16
Luc van Kessel <lucv.k@outlook.com>
2022-10-29 14:45:34 +0200

    save to json file working

--- cmd/klippa-assessment/main.go
+++ cmd/klippa-assessment/main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"assessment/internal/structs"
 	"assessment/internal/klippa-api"
+	"assessment/internal/structs"
+	// "encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	// "net/http/httputil"
 	"os"
 )
 
 func main() {
 
+	// Initialize the flags, this is done through the flag package provided by GO
 	ApiKey := flag.String("api", "[REQUIRED]", "a api key")
 	Template := flag.String("template", "financial_full", "which default provided template to use")
 	ExtractionType := flag.String("textextraction", "fast", "what thype of text extraction you want to use, needs to be fast or full")
 	FilePath := flag.String("file", "[REQUIRED]", "a document or image file path")
-	SaveFile := flag.String("save", "output", "name of how you want to save the result")
+	SaveFile := flag.String("save", "filename", "name of how you want to save the result")
 
 	flag.Parse()
-
-	// print all flags into the console.
-	// fmt.Println("ApiKey", *ApiKey)
-	// fmt.Println("Template", *Template)
-	// fmt.Println("ExtractionType", *ExtractionType)
-	// fmt.Println("FilePath", *FilePath)
-	// fmt.Println("SaveFile", *SaveFile)
 
 	// check if the file mentioned in the filepath exists
 	if _, err := os.Stat(string(*FilePath)); errors.Is(err, os.ErrNotExist) {
@@ -40,9 +38,31 @@ func main() {
 	requestconfig.FilePath = string(*FilePath)
 	requestconfig.SavefileName = string(*SaveFile)
 
-	result, err := klippaApi.ParseDocument(requestconfig)
+	// give the request config to the ParseDocument function, this will execute the api call.
+	response, err := klippaApi.ParseDocument(requestconfig)
 
+	// check if the given savefile name isnt the default one. if it is different we save it to a file.
+	if requestconfig.SavefileName != "filename" {
+		SaveResponse(response, *requestconfig)
+	}
+
+	// print the response to the console.
 	fmt.Println(err)
-	fmt.Println(result)
+
+}
+
+// saves the response body of a request response to a given filename.
+func SaveResponse(response *http.Response, requestconfig structs.RequestConfig) {
+	filename := requestconfig.SavefileName + ".json"
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	_ = os.WriteFile(filename, body, 0644)
+}
+
+// This function will pretty print the result in the console.
+func PrintResponse(response *http.Response) {
 
 }
--- internal/klippa-api/ParseDocument.go
+++ internal/klippa-api/ParseDocument.go
@@ -11,26 +11,26 @@ import (
 	"path/filepath"
 )
 
-// this function will make the parsedocument request to the klippa api
+// Calls the klippa parsedocument api
 func ParseDocument( requestconfig *structs.RequestConfig ) (*http.Response, error) {
 
-
+	// Opens the provided file that needs parsing
 	file, _ := os.Open(requestconfig.FilePath)
-
 	defer file.Close()
 
+	// Writing all the body elements of the request.
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 	part, _ := writer.CreateFormFile("document", filepath.Base(file.Name()))
 	io.Copy(part, file)
-	
 	extraction, err := writer.CreateFormField("pdf_text_extraction")
 	if err != nil {
 		return nil, err
 	}
-	extraction.Write([]byte(fmt.Sprintf(requestconfig.ExtractionType)))
+	extraction.Write([]byte(requestconfig.ExtractionType))
 	writer.Close()
 
+	// Creating a request and giving it some headers that are needed.
 	r,_ := http.NewRequest(
 		"POST",
 		fmt.Sprintf("https://custom-ocr.klippa.com/api/v1/parseDocument/%s", requestconfig.Template),
@@ -38,9 +38,11 @@ func ParseDocument( requestconfig *structs.RequestConfig ) (*http.Response, erro
 	r.Header.Add("Content-Type", writer.FormDataContentType())
 	r.Header.Add("X-Auth-Key", requestconfig.ApiKey)
 
+	// Execute the api call.
     resp, err := http.DefaultClient.Do(r)
     if err != nil {
         fmt.Println(err)
+		os.Exit(0)
     }
 
 	return resp, err
